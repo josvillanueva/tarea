1a415cc63fab48f1b383f93d9c345576ca7ce277
joseM-21T <A01733838@tec.mx>
2025-04-07 15:21:29 -0600

    jerarquía ejemplo

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print("Este es el main")
+
+num1 = 100
+num2 = 50
+num3 = 25
+num4 = 10
+
+print(num1 + num2 * num3)
+
+print((num1 + num2) * num3 - num4)
+
+print((num1 + num2) * (num3 - num4) / (num1 - num1))

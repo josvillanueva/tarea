e39de8d67285b6940cea051ba09c4d2723510248
Pablo-GvW <a01737407@tec.mx>
2025-04-07 16:47:54 -0600

    pablo

--- main.py
+++ main.py
@@ -15,3 +15,12 @@ print((num1 + num2) * (num3 - num4) / (num1 - num1))
 def resta(a,b):
     return a-b
 
+
+cuenta = float(input('introduzca el monto de la cuenta: '))
+propina = float(input('introduzca el porcentaje sin signos, 10 o 15 porciento: '))
+
+cuenta_con_propina = cuenta + (cuenta * (propina/100))
+
+print(cuenta_con_propina)
+
+print('esta es la cuenta total.')
